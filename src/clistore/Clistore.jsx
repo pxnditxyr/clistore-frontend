6aa2fc4eebb33d00793627a58c1e2a8685467ee3
Yuki xD <pxnditxyr@gmail.com>
2022-04-02 23:42:01 -0400

    Initializing Project Clistore App | React

---
+++ src/clistore/Clistore.jsx
@@ -0,0 +1,9 @@
+
+export const Clistore = () => {
+    return (
+        <>
+            <h1> Clistore </h1>
+            <hr />
+        </>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+import { Clistore } from './clistore/Clistore';
+
+import './index.css';
+
+ReactDOM.render(
+    <Clistore />,
+    document.getElementById( 'root' )
+);
